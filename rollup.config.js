a4b99fe79540a295f9b76e450810fab45e6a47b7
KWangechi <rachel.karanja@strathmore.edu>
2025-05-01 01:34:05 +0300

    chore: removed unnecessary attributes in tsconfig file

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from "rollup-plugin-typescript2";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
+import replace from "@rollup/plugin-replace";
 
 export default {
   input: "src/index.ts",
@@ -14,6 +15,10 @@ export default {
   ],
   plugins: [
     nodeResolve({ preferBuiltins: true }),
+    replace({
+      preventAssignment: true,
+      'process.env.BABEL_8_BREAKING': JSON.stringify(false),
+    }),
     commonjs(),
     typescript({ tsconfig: "./tsconfig.json" }),
     terser()
